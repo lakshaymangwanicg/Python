1c25ce3e74ac5d64efdf5b890a4d21b7263ecedd
lakshaymangwanicg <lakshaymangwani.cg@gmail.com>
2026-08-21 17:00:29 +0530

    Added python intro assignment 01

---
+++ 01-python-introduction/about_python.py
@@ -0,0 +1,6 @@
+# Python is a popular, high-level, and beginner-friendly programming language 
+# known for its clear syntax, readability, and multi-purpose versatility.
+
+print("Welcome to your Python journey!")
+print("Learning Python is a fantastic choice because its readable code makes programming fun and accessible.")
+print("With Python, you can explore exciting fields like web development, data science, and artificial intelligence.")
---
+++ 01-python-introduction/welcome.py
@@ -0,0 +1,2 @@
+# Displays a friendly welcome message to users when they start the program.
+print("Welcome Everyone.")
